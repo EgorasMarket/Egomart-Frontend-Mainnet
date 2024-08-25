5c0e2762bf730dee129039f96ccc0bf06c9409d6
kinglsey <goodluckcanhelp@gmail.com>
2024-08-25 15:43:54 +0100

    new

--- src/core/routes.js
+++ src/core/routes.js
@@ -1,3 +1,4 @@
 export const USER_TRADE_DEPOSIT_ROUTE = "/web3/get-event-deposit";
 export const GET_TICKER_PAIRS_ROUTE = "/web3/get-event-added-asset";
 export const GET_USER_TRADE_ORDERS_ROUTE = "/web3/get-event-exchange";
+export const GET_ALL_ORDERS_ROUTE = "/web3/get-all-event-exchange";
