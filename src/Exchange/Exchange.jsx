029b30c7cfe8cbcedf6dc7060db01b620b94e2c2
CYNTAX <samuelify225@gmail.com>
2024-08-27 12:47:48 +0100

    update

--- src/Exchange/Exchange.jsx
+++ src/Exchange/Exchange.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import ExchangeHeader from "./ExchangeHeader/ExchangeHeader";
 import ExchangeFooter from "./ExchangeFooter/ExchangeFooter";
 import { Outlet } from "react-router-dom";
+import { GET_TICKER_PAIRS } from "../services/trade.services";
+import { setTickers } from "../features/PairsSlice";
+import { useDispatch } from "react-redux";
 
 const Exchange = () => {
+  const dispatch = useDispatch();
+  const fetchTickers = async () => {
+    const res = await GET_TICKER_PAIRS();
+    if (!res?.success) return;
+
+    //loop through the record
+
+    const array = [];
+    let payload = {};
+
+    res.data.forEach((ticker) => {
+      payload = {
+        id: ticker.id,
+        img: ticker?.img,
+        pair: ticker.ticker,
+        OpenPrice: parseFloat(ticker.initialPrice).toFixed(2),
+        tickerA: ticker.tokenA,
+        tickerB: ticker.tokenB,
+        tickerBName: ticker.tokenBName,
+        tokenName: ticker.tokenAName,
+        change24h: 0,
+        open24h: 1000,
+        volume24h: 10,
+
+        // "{"website": "https://egochain.org",
+
+        meta: JSON.parse(ticker?.meta),
+      };
+      array.push(payload);
+    });
+
+    await dispatch(setTickers(array));
+  };
+
+  useEffect(() => {
+    fetchTickers();
+  }, []);
   return (
     <div className="ExchangeDiv">
       <ExchangeHeader />
