b2b0e17dfb4a9c4172ce1f1a7474f9046e76b6fa
CYNTAX <samuelify225@gmail.com>
2024-09-03 18:12:38 +0100

    update

--- src/Exchange/ExchangePages/ExchangeTrade/MarketDepth/MarketDepth.jsx
+++ src/Exchange/ExchangePages/ExchangeTrade/MarketDepth/MarketDepth.jsx
@@ -96,71 +96,73 @@ const MarketDepth = () => {
 
   return (
     <div className="marketDepthDiv">
-      <div className="marketDepthDiv_depth1">
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={130}
-            height={10}
-            data={sortedBuyOffers}
-            margin={{
-              top: 0,
-              right: 0,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <defs>
-              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="1%" stopColor="#51cb89" stopOpacity={0.2} />
-                <stop offset="100%" stopColor="#51cb89" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            {/* <CartesianGrid strokeDasharray="3 3" stroke="#fff" opacity={0.2} /> */}
-            {/* <XAxis dataKey="price" stroke="0" /> */}
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="amount"
-              stroke="#22ad62"
-              fillOpacity={1}
-              fill="url(#colorUv)"
-              strokeWidth={2}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
-      <div className="marketDepthDiv_depth2">
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={130}
-            height={10}
-            data={sortedSellOffers}
-            margin={{
-              top: 0,
-              right: 0,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <defs>
-              <linearGradient id="colorUv2" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="1%" stopColor="#de394f" stopOpacity={0.2} />
-                <stop offset="100%" stopColor="#de394f" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            {/* <CartesianGrid strokeDasharray="3 3" stroke="#fff" opacity={0.2} /> */}
-            {/* <XAxis dataKey="price" stroke="0" /> */}
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="amount"
-              stroke="#ff535b"
-              fillOpacity={1}
-              fill="url(#colorUv2)"
-              strokeWidth={2}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
+      <div className="marketDepthDiv_area">
+        <div className="marketDepthDiv_depth1">
+          <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+              width={130}
+              height={10}
+              data={sortedBuyOffers}
+              margin={{
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="1%" stopColor="#51cb89" stopOpacity={0.2} />
+                  <stop offset="100%" stopColor="#51cb89" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              {/* <CartesianGrid strokeDasharray="3 3" stroke="#fff" opacity={0.2} /> */}
+              {/* <XAxis dataKey="price" stroke="0" /> */}
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="amount"
+                stroke="#22ad62"
+                fillOpacity={1}
+                fill="url(#colorUv)"
+                strokeWidth={2}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="marketDepthDiv_depth2">
+          <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+              width={130}
+              height={10}
+              data={sortedSellOffers}
+              margin={{
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <defs>
+                <linearGradient id="colorUv2" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="1%" stopColor="#de394f" stopOpacity={0.2} />
+                  <stop offset="100%" stopColor="#de394f" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              {/* <CartesianGrid strokeDasharray="3 3" stroke="#fff" opacity={0.2} /> */}
+              {/* <XAxis dataKey="price" stroke="0" /> */}
+              <Tooltip />
+              <Area
+                type="monotone"
+                dataKey="amount"
+                stroke="#ff535b"
+                fillOpacity={1}
+                fill="url(#colorUv2)"
+                strokeWidth={2}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </div>
   );
