5cf5391883d20aaa31f40f1ed7f1f87df28ee270
kinglsey <goodluckcanhelp@gmail.com>
2024-09-14 09:31:29 +0100

    update

---
+++ src/helpers/helper.js
@@ -0,0 +1,13 @@
+export const _priceChangeStyling = ({ pair }) => {
+  //destructure thr pair
+  console.log(pair?.open24h, pair?.close24h, pair, "loko");
+
+  if (parseFloat(pair?.open24h) > parseFloat(pair?.close24h)) {
+    return "#ff445d";
+  }
+
+  if (parseFloat(pair?.open24h) < parseFloat(pair?.close24h)) {
+    return " #12b66f";
+  }
+  return "#fff";
+};
