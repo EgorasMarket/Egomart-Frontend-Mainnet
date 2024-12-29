f1e94e78627dfb500f841cb43ed50829575a87c5
kinglsey <goodluckcanhelp@gmail.com>
2024-12-29 23:52:43 +0100

    modified contract

--- dist/assets/index-5SvjEW5h.js
+++ dist/assets/index-5SvjEW5h.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-Bq9cJxxH.js";import{bg as Ur}from"./index-atig05hy.js";/**
+import{n as fe,s as Ke,T as k,t as P,a as g,o as Y,R as Dr,p as H,y as ae}from"./index-47q1ve_0.js";import{bg as Ur}from"./index-B0QkigoY.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
